b7ffc8c3efb070be41932285fcb1d96c67ee55ca
Gleb Nosov <nklokk.mail@gmail.com>
2025-10-24 23:06:35 +0000

    migrate plugins

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -9,3 +9,21 @@ vim.api.nvim_create_autocmd(
     end,
   }
 )
+
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    pattern = {"lua", "python", "c", "cpp", "markdown"},
+    callback = function()
+      vim.wo.foldmethod = "expr"
+      vim.wo.foldlevel = 99
+      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+      vim.bo.autoindent = true
+      vim.bo.smartindent = false
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+      vim.treesitter.start()
+    end,
+  }
+)
